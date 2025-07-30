996b7a72ae9f787cf1382c95a6dfa1426706bc46
kalaivanan564 <kalaivananeswaran564@gmail.com>
2025-07-30 12:26:03 +0530

    importexport

---
+++ class 21 import export/one.js
@@ -0,0 +1,12 @@
+let tax = 18;
+function get_discount(){
+    return 10;
+ }
+ class employee{
+   constructor(){
+        console.log("central file - employee class");
+          }
+
+ }
+
+ export default tax;
